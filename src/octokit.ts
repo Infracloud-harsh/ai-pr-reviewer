4249135046166f04550201aba82cd8df41b5e641
Infracloud-harsh <148188551+Infracloud-harsh@users.noreply.github.com>
2023-11-29 20:34:05 +0530

    Update octokit.ts

--- src/octokit.ts
+++ src/octokit.ts
@@ -8,37 +8,37 @@ const token = getInput('token') || process.env.GITHUB_TOKEN
 const RetryAndThrottlingOctokit = Octokit.plugin(throttling, retry)
 
 export const octokit = new RetryAndThrottlingOctokit({
-    auth: `token ${token}`,
-    throttle: {
+  auth: `token ${token}`,
+  throttle: {
     onRateLimit: (
-        retryAfter: number,
-        options: any,
-        _o: any,
-        retryCount: number
-      ) => {
-        warning(
-            `Request quota exhausted for request ${options.method} ${options.url}
+      retryAfter: number,
+      options: any,
+      _o: any,
+      retryCount: number
+    ) => {
+      warning(
+        `Request quota exhausted for request ${options.method} ${options.url}
 Retry after: ${retryAfter} seconds
 Retry count: ${retryCount}
 `
-          )
-          if (retryCount <= 3) {
-            warning(`Retrying after ${retryAfter} seconds!`)
-            return true
-          }
-        },
-        onSecondaryRateLimit: (retryAfter: number, options: any) => {
-          warning(
-            `SecondaryRateLimit detected for request ${options.method} ${options.url} ; retry after ${retryAfter} seconds`
-          )
-          // if we are doing a POST method on /repos/{owner}/{repo}/pulls/{pull_number}/reviews then we shouldn't retry
-          if (
-            options.method === 'POST' &&
-            options.url.match(/\/repos\/.*\/.*\/pulls\/.*\/reviews/)
-          ) {
-            return false
-          }
-          return true
-        }
+      )
+      if (retryCount <= 3) {
+        warning(`Retrying after ${retryAfter} seconds!`)
+        return true
       }
-    })
+    },
+    onSecondaryRateLimit: (retryAfter: number, options: any) => {
+      warning(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url} ; retry after ${retryAfter} seconds`
+      )
+      // if we are doing a POST method on /repos/{owner}/{repo}/pulls/{pull_number}/reviews then we shouldn't retry
+      if (
+        options.method === 'POST' &&
+        options.url.match(/\/repos\/.*\/.*\/pulls\/.*\/reviews/)
+      ) {
+        return false
+      }
+      return true
+    }
+  }
+})
